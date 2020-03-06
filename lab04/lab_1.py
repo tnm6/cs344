910b626945cb7501fa95f3654b2d0ed99310266f
Nathan <nathan@The-Normandy.local>
2020-03-05 22:05:15 -0500

    4.1 done

--- lab04/lab_1.py
+++ lab04/lab_1.py
@@ -20,3 +20,31 @@ P[F, F, T] = 0.144; P[F, F, F] = 0.576
 # Compute P(Cavity|Toothache=T)  (see the text, page 493).
 PC = enumerate_joint_ask('Cavity', {'Toothache': T}, P)
 print(PC.show_approx())
+
+# Compute P(Cavity|Catch=T)
+'''
+Hand calculated P(Cavity|Catch=T)
+P(Cavity|Catch) = P(Cavity, Catch) / P(Catch)
+= (0.108 + 0.072) / (0.108 + 0.072 + 0.016 + 0.144)
+= 0.529
+So hand calculation is: False: 0.471, True: 0.529
+'''
+PC = enumerate_joint_ask('Cavity', {'Catch': T}, P)
+print(PC.show_approx())
+
+# Computation result is: False: 0.471, True: 0.529
+
+# Joint Probability Distribution for flipping two coins
+P = JointProbDist(['Coin1', 'Coin2'])
+H, T = 'Heads', 'Tails'
+P[H, H] = 0.25; P[H, T] = 0.25
+P[T, H] = 0.25; P[T, T] = 0.25
+
+# Compute P(Coin2|Coin1=Heads)
+PC = enumerate_joint_ask('Coin2', {'Coin1': T}, P)
+print(PC.show_approx())
+
+# Computation result: Heads: 0.5, Tails: 0.5
+# This is what I expected, since flipping each coin is
+# independent of the other coinflip, i.e. each is 0.5
+
