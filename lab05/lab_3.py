2df649b0e40494ca30fe4f4eb54842cff13cf537
Nathan <nathan@The-Normandy.local>
2020-03-08 23:21:29 -0400

    Partial lab 5 submission

--- lab05/lab_3.py
+++ lab05/lab_3.py
@@ -23,23 +23,15 @@ happiness = BayesNet([
 print('P(Raise | Sunny):')
 print('\t' + enumeration_ask('Raise', dict(
     Sunny=T), happiness).show_approx())
-# Result: False: 0.99, True: 0.01 (?!?!?)
+# Result: False: 0.99, True: 0.01
+
+'''
+This result makes sense. Since Raise and Sunny are independent, Raise | Sunny
+is equal to Raise, which is 0.01, or 1%.
+'''
 
 # Compute P(Raise | Happy ^ Sunny)
 print('P(Raise | Happy ^ Sunny):')
 print('\t' + enumeration_ask('Raise', dict(
     Happy=T, Sunny=T), happiness).show_approx())
 # Result: False: 0.986, True: 0.0142
-
-'''
-  These results are a bit perplexing, mostly because, at least by my own reason
-(that's dangerous enough), being sunny wouldn't affect the chance of a raise.
-The Bayesian network suggests that they are independent of each other, but I
-suppose the small (1%) probability of a raise is taking into account the
-(maybe) coincidental correlation that a raise occured on the same day? But that
-is not the probability being calculated...
-
-P(Raise | Sunny) = α <P(Raise | Sunny), P(¬Raise | Sunny)>
-= α <( P(Sunny | Raise) * P(Raise) ), ( P(Sunny | ¬Raise) * P(¬Raise) )>
-= 
-'''
