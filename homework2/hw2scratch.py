30067c01f545e11b749456cdb01002273de403d1
Nathan <nathan@DESKTOP-F3NHA5U.localdomain>
2020-03-10 19:47:47 -0400

    Added scratch file for hw2

---
+++ homework2/hw2scratch.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+spam_corpus = [["I", "am", "spam", "spam", "I", "am"], ["I", "do", "not", "like", "that", "spamiam"]]
+ham_corpus = [["do", "i", "like", "green", "eggs", "and", "ham"], ["i", "do"]]
+
+spam_count = dict(Counter(spam_corpus))
+print(spam_count)
