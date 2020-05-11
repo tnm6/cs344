24adb6fff6711820dee646e7a26c86fc9efbf51f
Nathan <nathan@The-Normandy.local>
2020-05-10 23:17:07 -0400

    13.1.b?

--- lab13/lab_1.pl
+++ lab13/lab_1.pl
@@ -3,7 +3,7 @@
  * Answers by Nathan Meyer (tnm6)
  */
 
-% Exercises from LPN!
+% a. Exercises from LPN!
 %   i.  Exercise 3.2
 directlyIn(natasha, irina).
 directlyIn(olga, natasha).
@@ -38,3 +38,15 @@ listtran([X|Tx], [Y|Ty]):- tran(X, Y),
 % resolves to X = [one, nine, two].
 ?- listtran(X,[one,seven,six,two]).
 % resolves to X = [eins, sieben, sechs, zwei].
+
+
+% b. Does Prolog implement a version of generalized modus ponens?
+% Yes, it does. Given this simple knowledge base:
+flies(X):- plane(X).
+plane(747).
+% where plane(X) implies flies(X), and plane(747) is true, so
+?- flies(747).
+% also returns true. And
+?- flies(X).
+% returns 747, using the variable X and instantiating it to the first (and only)
+% result which satisfies the rule plane(X), X = 747.
