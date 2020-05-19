8bbe66322fb6394ce430a301543260745556a8d3
Nathan <nathan@The-Normandy.local>
2020-05-19 01:21:55 -0400

    YOU'VE GOT THE FEATURES?!

--- project/cornell.py
+++ project/cornell.py
@@ -1,6 +1,27 @@
 import sqlite3 as sql
 import numpy as np
+from keras.preprocessing.text import Tokenizer
+
+NUM_WORDS = 10000
 
 data = sql.connect("movie_lines.db")
 cursor = data.cursor()
 
+cursor.execute("SELECT line_text FROM lines")
+
+# fetchall() always returns a tuple, so keep only first index of each tuple
+lines = np.array(cursor.fetchall())[:,0]
+print(lines[:10])
+
+tokenizer = Tokenizer(num_words=NUM_WORDS)
+tokenizer.fit_on_texts(lines)
+
+lines_by_movie = []
+cursor.execute("SELECT COUNT(movie_id) FROM movies")
+for i in range(cursor.fetchone()[0]):
+    cursor.execute("SELECT line_text FROM lines WHERE movie_id = {}".format(i))
+    cur_lines = np.array(cursor.fetchall())[:,0]
+    cur_sequence = tokenizer.texts_to_sequences(cur_lines)
+    lines_by_movie.append(cur_sequence)
+features = np.array(lines_by_movie)
+print(features[0])
